675351ab1b3bbeb576098d0b7024c43b7526e2fd
André Walker <andre@andrewalker.net>
2013-07-30 17:33:43 -0300

    Create basic methods for the public pages of company

--- lib/PerlPro/Web/Controller/Public/Company.pm
+++ lib/PerlPro/Web/Controller/Public/Company.pm
@@ -5,10 +5,46 @@ use utf8;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-sub index :Path Args(0) {
-    my ( $self, $c ) = @_;
+sub base :Chained('/') PathPart('company') CaptureArgs(0) {
+    my ( $self, $ctx ) = @_;
+    $ctx->stash(
+        current_model_instance => $ctx->model('DB::Company')
+    );
+}
 
-    $c->response->body('Matched PerlPro::Web::Controller::Public::Company.');
+sub profile :Chained('base') PathPart('') Args(1) GET {
+    my ( $self, $ctx, $company ) = @_;
+
+    $ctx->stash(
+        item => $ctx->model->find($company)
+    );
+}
+
+sub catalog :Chained('base') PathPart('') Args(0) GET {
+    my ( $self, $ctx ) = @_;
+
+    # TODO: paging
+    # This is obviously dangerous as it could potentially lock the database and
+    # completely fill memory, but it's good enough for a MVP, as we don't
+    # expect millions of companies too soon.
+    # But it should be fixed soon.
+    $ctx->stash(
+        companies => [ $ctx->model->search->all ]
+    );
+}
+
+sub register :Chained('base') Does('DisplayExecute') Args(0) {}
+
+sub register_display {
+    my ( $self, $ctx ) = @_;
+}
+
+sub register_execute {
+    my ( $self, $ctx ) = @_;
+
+    # TODO
+    # We could deal with DataManager here, if necessary. Although
+    # DisplayExecute already does that automatically if we want.
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -31,7 +67,29 @@ Controller for browsing companies, and registering a new company.
 
 =head1 METHODS
 
-=head2 index
+=head2 base
+
+Top of the chain of all methods in this class.
+
+=head2 profile
+
+Display the profile of a given company.
+
+=head2 catalog
+
+Display a list of all companies registered.
+
+=head2 register
+
+Register a new company.
+
+=head2 register_display
+
+Show the template with the form to register a new company.
+
+=head2 register_execute
+
+Save the form.
 
 =head1 AUTHOR
 
