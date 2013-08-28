1d5e2dde2edfb4535eb50227923dd5dc2d35c45d
André Luiz Ferreira <andre@nova1.com.br>
2013-08-28 11:37:05 -0300

    Create job search results template

--- root/static/js/main.js
+++ root/static/js/main.js
@@ -18,4 +18,48 @@ jQuery(function ($) {
             event: 'click'
         }
     });
+
+
+    $('.one-filter').each( function(){
+
+        $( this ).qtip({
+            content: {
+                title: $( '.filter-title', this ).html(),
+                text: $( '.filter-content', this )
+            },
+            style: {
+                classes: 'qtip-bootstrap'
+            },
+            position: {
+                at: 'bottom center',
+                my: 'top center'
+            },
+            show: {
+                event: 'click'
+            },
+            hide: {
+                event: 'click'
+            },
+            events: {
+                show: function() {
+                    $('.qtip').hide();
+                }
+            }
+        });
+
+    });
+
+    $( '.add-filter' ).click( function() {
+        var value = $( this ).parent().find( 'input[type="text"]' ).val();
+
+        if( value != '' ){
+            var html = '<li>'+value+' <i class="icon-remove-sign"></i></li>';
+            $( this ).parent().parent().find( '.selected-filters' ).append( html );
+
+            $( '.icon-remove-sign' ).click( function() {
+                $( this ).parent( 'li' ).remove();
+            });
+        }
+    });
+
 });
