7858bb79ccc8fd01ca029d2025ee75f9a2679d06
André Walker <andre@andrewalker.net>
2013-08-29 20:30:34 -0300

    Create JSON view

---
+++ lib/PerlPro/Web/View/JSON.pm
@@ -0,0 +1,36 @@
+package PerlPro::Web::View::JSON;
+
+use base 'Catalyst::View::JSON';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlPro::Web::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<PerlPro::Web>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+André Walker
+
+=head1 LICENSE
+
+This file is part of PerlPro.
+
+PerlPro is free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
---
+++ t/view_JSON.t
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN { use_ok 'PerlPro::Web::View::JSON' }
+
+done_testing();
