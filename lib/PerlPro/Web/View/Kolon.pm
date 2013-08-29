2c62c97163a9b0dff92b8b4e86c5c8a7706f1ebc
André Walker <andre@andrewalker.net>
2013-08-29 20:31:51 -0300

    Add some documentation to Kolon view

--- lib/PerlPro/Web/View/Kolon.pm
+++ lib/PerlPro/Web/View/Kolon.pm
@@ -54,3 +54,48 @@ sub fillinform {
 __PACKAGE__->meta->make_immutable;
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlPro::Web::View::Kolon - Catalyst Xslate View
+
+=head1 SYNOPSIS
+
+See L<PerlPro::Web>
+
+=head1 DESCRIPTION
+
+Catalyst Xslate view, using the Kolon templating language.
+
+=head1 METHODS
+
+=head2 ACCEPT_CONTEXT
+
+=head2 localize
+
+=head2 fillinform
+
+=head2 view_uri_for
+
+=head1 SEE ALSO
+
+L<Catalyst::View::Xslate>, L<Text::Xslate>, L<Text::Xslate::Syntax::Kolon>
+
+=head1 AUTHOR
+
+André Walker
+
+=head1 LICENSE
+
+This file is part of PerlPro.
+
+PerlPro is free software: you can redistribute it and/or modify it under the
+terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
